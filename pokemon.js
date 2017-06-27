e0b747dc96c45a379e044e881a35ef8eadcb29d8
Artemio Cabelin <artemiocabelin@gmail.com>
2017-06-27 16:26:51 -0700

    added pokemon html css and js files

---
+++ pokemon.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    for(var i=1;i<= 151;i++){
+        $('#main_wrapper').append('<img class=\"box\" src=\"http://pokeapi.co/media/img/'+i+'.png\" alt=\"img'+i+'\">');
+    }
+
+});
